8693967f7859b77ae575d65f65ccdaac7fdd3bed
master <josemartin.lopez.blanco@gmail.com>
2019-02-11 14:13:36 +0100

    creada clase posts

---
+++ AppIndv/AppIndv/Posts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppIndv
+{
+    class Posts
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
