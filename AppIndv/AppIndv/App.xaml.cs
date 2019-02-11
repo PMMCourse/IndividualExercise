5f7ec7dcee7c10d0ec220588a0cdfc5c1742efb7
master <josemartin.lopez.blanco@gmail.com>
2019-02-11 14:23:16 +0100

    todo terminado

--- AppIndv/AppIndv/App.xaml.cs
+++ AppIndv/AppIndv/App.xaml.cs
@@ -11,7 +11,7 @@ namespace AppIndv
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage (new MainPage());
         }
 
         protected override void OnStart()
--- AppIndv/AppIndv/MainPage.xaml.cs
+++ AppIndv/AppIndv/MainPage.xaml.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace AppIndv
 {
     public partial class MainPage : ContentPage
     {
+        private const string url = "https://jsonplaceholder.typicode.com/posts";
+        private HttpClient client = new HttpClient();
+        private List<Posts> postsList;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            var content = await client.GetStringAsync(url);
+            var post = JsonConvert.DeserializeObject<List<Posts>>(content);
+            postsList = new List<Posts>(post);
+            Post_List.ItemsSource = postsList;
+            base.OnAppearing();
+        }
+
+        async private void MainListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var Selected = e.Item as Posts;
+            await Navigation.PushAsync(new PagePost(Selected));
+
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                Post_List.ItemsSource = postsList;
+            }
+            else
+            {
+                Post_List.ItemsSource = postsList.Where(x => x.title.Contains(e.NewTextValue));
+            }
         }
     }
 }
