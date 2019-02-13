587967b5ebcfb948cf631bdf3d0b047b636faf85
Pablo Vazquez Diaz <pablo.vaz.diaz@gmail.com>
2019-02-13 09:23:24 +0100

    View Navigation

--- EjercicioIndividual/EjercicioIndividual/View/MainPage.xaml.cs
+++ EjercicioIndividual/EjercicioIndividual/View/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using EjercicioIndividual.ViewModel;
+﻿using EjercicioIndividual.Model;
+using EjercicioIndividual.View;
+using EjercicioIndividual.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +17,16 @@ namespace EjercicioIndividual
 			InitializeComponent();
             this.BindingContext = new MainPageViewModel();
 		}
-	}
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var post = e.SelectedItem as Post;
+            if (post == null)
+                return;
+            await Navigation.PushAsync(new PostDetails());
+
+            PostsListView.SelectedItem = null;
+
+        }
+    }
 }
