8adcbc5f13b7e884ee23f76754835654a99aa597
[maruedam] <[miuelangelruedamuriel@gmail.com]>
2019-02-13 19:24:07 +0100

    fin del ejercicio sin mvvm

---
+++ ejercicio_ma/ejercicio_ma/Classes/ListViewTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio_ma.Classes
+{
+    public class ListViewTemplate
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int OrderNumber { get; set; }
+
+    }
+}
---
+++ ejercicio_ma/ejercicio_ma/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicio_ma
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(Post p)
+
+        {
+
+            InitializeComponent(); titulo.Text = p.title;
+
+            contenido.Text = p.body; userId.Text = $"user id{p.userId}";
+
+            id.Text = $"id {p.id}";
+
+        }
+
+        public void mensaje(object sender, EventArgs e)
+
+        {
+
+            this.DisplayAlert("", "thanks for your comment", "ok");
+
+        }
+    }
+    
+}
