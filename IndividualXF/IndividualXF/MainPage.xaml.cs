728c6f73732ceb0506e653d48fde229a84abdceb
CrishFK <cristiansanchez93@hotmail.es>
2019-02-13 23:19:52 +0100

    Ejercicio terminado

---
+++ IndividualXF/IndividualXF/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+
+namespace IndividualXF
+{
+    public partial class MainPage : ContentPage
+    {
+
+        List<Post> listaPosts = new List<Post>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            HttpClient httpClient = new HttpClient();
+            string postsJSON = httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts").Result;
+            listaPosts = JsonConvert.DeserializeObject<List<Post>>(postsJSON);
+            lbPost.ItemsSource = listaPosts;
+        }
+
+
+        private void LbPost_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Navigation.PushAsync(new ViewPost(e.SelectedItem as Post));
+        }
+
+        private void SbPost_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(sbPost.Text.ToLower()))
+            {
+                var postFiltrados = listaPosts.Where(post => post.Title.ToLower().StartsWith(sbPost.Text.ToLower())).ToList();
+                lbPost.ItemsSource = null;
+                lbPost.ItemsSource = postFiltrados;
+            }
+            else
+            {
+                lbPost.ItemsSource = listaPosts;
+            }
+        }
+    }
+}
---
+++ IndividualXF/IndividualXF/ViewPost.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IndividualXF
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewPost : ContentPage
+	{
+
+        public ViewPost (Post articulo)
+		{
+			InitializeComponent ();
+            Id.Text = "User Id: "+articulo.UserId.ToString()+"  Post Id: "+articulo.Id.ToString();
+            Title.Text = articulo.Title;
+            Body.Text = articulo.Body;
+        }
+
+        private void btComment(object sender, EventArgs e)
+        {
+            DisplayAlert("","Gracias por su comentario","","OK");
+        }
+    }
+}
