f4b7af02995c1ac679f768b82b334d7cb68e343f
Jose Panadero <opanaleiro@hotmail.com>
2019-02-27 09:01:26 +0100

    Lista simple

--- IactivityPana1/IactivityPana1/IactivityPana1/Model/RootObject.cs
+++ IactivityPana1/IactivityPana1/IactivityPana1/Model/RootObject.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace IactivityPana1.Model
 {
-    public class RootObject
+    public class RootObject 
     {
         public int userId { get; set; }
         public int id { get; set; }
         public string title { get; set; }
         public string body { get; set; }
-        public string idtitle { get { return id + " " + title; }}
-
+        public string idtitle { get { return id + " " + title; }}      
 
     }
 
